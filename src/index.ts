d30d7ddf2883f8226d1d7f559f8da88fbcebad17
itzdrli <liletian1231@outlook.com>
2024-03-05 20:07:14 +0000

    updated usage

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ export const Config: Schema<Config> = Schema.object({
 
 export const usage = `
 <h2>如遇使用问题可以前往QQ群: 957500313 讨论<h2>
-<h2>基岩版没有完整的支持，出现错误请关闭该功能</h2>`
+<h2>基岩版没有完整的支持，出现错误请关闭该功能或者前往Q群反馈</h2>`
 
 export function apply(ctx: Context, config: Config) {
   mcs(ctx, config)
