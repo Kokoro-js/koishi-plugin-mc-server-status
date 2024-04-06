7869ec6ba2ec5984e36ea99a23773806171b0fff
itzdrli <liletian1231@outlook.com>
2024-04-06 17:38:24 +0000

    2.2.6

--- src/commands/mcs.ts
+++ src/commands/mcs.ts
@@ -2,8 +2,8 @@ import { Context } from "koishi";
 import { Config } from '../index';
 import { } from 'koishi-plugin-puppeteer'
 
-export function generateHtml(result: string, cicon: boolean, server: string): string {
-    return `
+export function generateHtml(result: string, cicon: boolean, server: string, footer: string): string {
+  return `
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,13 +22,13 @@ export function generateHtml(result: string, cicon: boolean, server: string): st
 <body style="width: 650px">
   <div class="container mx-auto py-8">
     <div class="text-center">${cicon ? `<img src="https://sr-api.sfirew.com/server/${server}/icon.png" alt="icon"
-        class="w-35 h-35 mx-auto" />` : ''}</div>
+        class="w-50px h-50px mx-auto" />` : ''}</div>
     <div class="text-center mt-4">
       <div class="text-lg font-bold text-white">${result}</div>
     </div>
   </div>
   <footer class="bg-gray-800 text-center py-2">
-    <p class="text-sm text-gray-400">Powered by koishi-plugin-mc-server-manager v.2.2.5</br>广告位招租 @itzdrli</p>
+    <p class="text-sm text-gray-400">${footer}</p>
   </footer>
   </div>
 </body>
@@ -37,7 +37,6 @@ export function generateHtml(result: string, cicon: boolean, server: string): st
 }
 
 export function mcs(ctx: Context, config: Config) {
-  let result = ''
   ctx.command('mcs [server]', '查询Minecraft服务器状态', { authority: config.authority })
     .action(async ({ session }, server) => {
       if (!server) {
@@ -51,7 +50,7 @@ export function mcs(ctx: Context, config: Config) {
       if (!config.bedrockSupport) {
         result += `<p>版本: ${data.version?.raw} - ${data.version?.protocol}</p>`;
         if (config.motd && data.motd) {
-            result += `<p>${data.motd.html}</p>`;
+          result += `<p>${data.motd.html}</p>`;
         }
         result += `<p>在线人数: ${data.players?.online}/${data.players?.max}</p>`;
       } else {
@@ -71,8 +70,9 @@ export function mcs(ctx: Context, config: Config) {
         }
       }
 
-    const html = generateHtml(result, config.icon, server);
-    const image = await ctx.puppeteer.render(html);
-    return image;
+      const footer = config.footer.replace(/\n/g, '<br>')
+      const html = generateHtml(result, config.icon, server, footer);
+      const image = await ctx.puppeteer.render(html);
+      return image;
     })
 }
